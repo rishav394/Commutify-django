d44ecf72d01f8ae09cec8498abafc5ff212089e8
Rishav <rishav.ext@grofers.com>
2021-03-15 00:30:13 +0530

    Add swagger

--- commutify/urls.py
+++ commutify/urls.py
@@ -1,11 +1,26 @@
+from django.conf.urls import url
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from commutify.restapis import views
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    # Users
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path("users", views.users),
     path("login", views.login),
     path("sign-up", views.sign_up),
