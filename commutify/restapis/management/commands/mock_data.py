39067da7d8cf9745970303c4e3a5af57102f1363
Rishav <rishav.ext@grofers.com>
2021-03-28 22:21:24 +0530

    Fix mock data

--- commutify/restapis/management/commands/mock_data.py
+++ commutify/restapis/management/commands/mock_data.py
@@ -2,7 +2,7 @@ import json
 import re
 import urllib.parse
 import urllib.request
-from random import choice, randrange
+from random import choice, randrange, sample
 
 from commutify.restapis.models import (
     Domain,
@@ -31,26 +31,30 @@ class Command(BaseCommand):
     def get_random_fiendship_status(self):
         return FriendshipStatus.objects.get(id=randrange(2, 5))
 
-    def populate_users(self, count=100):
+    def populate_users(self, count=150):
         print("Populating {} users".format(count))
         url = "https://randomuser.me/api/?results={}".format(count)
         f = urllib.request.urlopen(url)
         user_data = UserResultfromdict(json.loads(f.read().decode("utf-8")))
         for result in user_data.results:
-            User.objects.create(
-                name=result.name.first,
-                email=result.email,
-                phone=re.sub("[^0-9]", "", result.phone)[:10],
-                password=get_hashed_password(result.email),
-                dob="{}-{}-{}".format(
-                    result.dob.date.year, result.dob.date.month, result.dob.date.day
-                ),
-                gender=self.get_random_gender(),
-                photo=result.picture.large,
-                bio="I am {} {} and my email is {}".format(
-                    result.name.first, result.name.last, result.email
-                ),
-            )
+            try:
+                User.objects.create(
+                    name=result.name.first,
+                    email=result.email,
+                    phone=re.sub("[^0-9]", "", result.phone)[:10],
+                    password=get_hashed_password(result.email),
+                    dob="{}-{}-{}".format(
+                        result.dob.date.year, result.dob.date.month, result.dob.date.day
+                    ),
+                    gender=self.get_random_gender(),
+                    photo=result.picture.large,
+                    bio="I am {} {} and my email is {}".format(
+                        result.name.first, result.name.last, result.email
+                    ),
+                )
+            except Exception as e:
+                print(e)
+                print(result.email)
         print("Populated {} users".format(count))
 
     def populate_domains(self):
@@ -59,14 +63,17 @@ class Command(BaseCommand):
         infos = list(map(lambda x: "Some information about {}".format(x), names))
 
         for i in range(len(names)):
-            Domain.objects.create(
-                name=names[i].capitalize(),
-                info=infos[i],
-                created_by=User.objects.order_by("?")[0],
-            )
+            try:
+                Domain.objects.create(
+                    name=names[i].capitalize(),
+                    info=infos[i],
+                    created_by=User.objects.order_by("?")[0],
+                )
+            except Exception as e:
+                print(names[i])
         print("Populated {} domains".format(len(names)))
 
-    def populate_user_domains(self, count=100):
+    def populate_user_domains(self, count=250):
         print("Populating {} user-domains".format(count))
         success_count = 0
         while success_count < count:
@@ -80,27 +87,29 @@ class Command(BaseCommand):
                 pass
         print("Populated {} user-domains".format(count))
 
-    def populate_user_friends(self, count=200):
-        print("Populating {} user-friends".format(count))
-        success_count = 0
-        while success_count < count:
-            try:
-                user1: User = User.objects.order_by("?")[0]
-                user2 = User.objects.order_by("?")[0]
-                if user1.id == user2.id:
-                    raise ""
-                if user1.id > user2.id:
-                    user1, user2 = user2, user1
-                UserFriend.objects.create(
-                    user1=user1,
-                    user2=user2,
-                    status=FriendshipStatus.objects.get(id=choice([3, 4])),
-                    initiator=choice([user1, user2]),
-                )
-                success_count += 1
-            except Exception as e:
-                pass
-        print("Populated {} user-friends".format(count))
+    def populate_user_friends(
+        self,
+    ):
+        print("Populating {} user-friends".format("some"))
+        user_count = User.objects.count()
+        counter = 0
+        for i in range(1, user_count):
+            to_friend = sample(range(i + 1, user_count), (user_count - i) // 2)
+            user1: User = User.objects.get(id=i)
+            for x in to_friend:
+                user2 = User.objects.get(id=x)
+
+                try:
+                    UserFriend.objects.create(
+                        user1=user1,
+                        user2=user2,
+                        status=FriendshipStatus.objects.get(id=choice([3, 4])),
+                        initiator=choice([user1, user2]),
+                    )
+                    counter += 1
+                except Exception as e:
+                    print(e)
+        print("Populated {} user-friends".format(counter))
 
     def handle(self, *args, **options):
         self.populate_users()
